38261c1ca927b66d33c9fc5bfdaa41ce3d9a184a
Carlos Gomes Cabral <carlos.gomescabral@gmail.com>
2021-11-23 08:40:23 -0300

    Initial Commit

---
+++ Showcase/ShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowcaseApp.swift
+//  Showcase
+//
+//  Created by Carlos Cabral on 23/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
